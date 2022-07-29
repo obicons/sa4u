9653b5dfe9486f5ba1aa37dc9de47c9cceb0662a
Ikutania <away993@yahoo.com>
2022-07-28 21:21:42 -0400

    Bug Fixes, Edits, and Updates
    Fixed:
    Issue #26, #25, #23, #19
    
    Refactored and reduced
    Placed Debug keys into a single object

--- lsp/server/src/server.ts
+++ lsp/server/src/server.ts
@@ -23,7 +23,7 @@ import { TextDocument } from 'vscode-languageserver-textdocument';
 
 import { exec, execSync } from 'child_process';
 import { promisify } from 'util';
-import { existsSync, promises } from 'fs';
+import { constants, promises } from 'fs';
 // Create a connection for the server, using Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
 const connection = createConnection(ProposedFeatures.all);
@@ -32,32 +32,79 @@ const connection = createConnection(ProposedFeatures.all);
 const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
 
 // Docker image to use for analysis.
-const dockerImageName = "sa4u/sa4u:0.7.0";
+const dockerImageName = 'sa4u/sa4u:0.7.0';
+
+// Location of config file
+const CONFIG_FILE = '.sa4u/config.json';
 
 let hasConfigurationCapability = false;
 let hasWorkspaceFolderCapability = false;
-let messDef: string;
 let allowConfigurationChanges = false;
 let startedSA4U_Z3 = false;
 
 const execAsync = promisify(exec);
 
-class SA4UConfig {
-	compilationDir: string
-	constructor(compilationDir: string) {
-		this.compilationDir = '/src/' + compilationDir;
+class SA4UDebug {
+	Image: string;
+	MaintainContainerOnExit: boolean;
+	constructor(image:string, maintainOnExit:boolean) {
+		this.Image = image;
+		this.MaintainContainerOnExit = maintainOnExit;
 	}
 }
 
-class SA4UIgnoredFiles {
-	ignoreFilesCommand: string
-	constructor(ignoreFilesArray: string[]) {
-		if (ignoreFilesArray.length !== 0) {
-			this.ignoreFilesCommand = '-i '.concat(ignoreFilesArray.join(' -i '));
+class SA4UConfig {
+	ProtocolDefinitionFile: string;
+	CompilationDir: string;
+	PriorTypes: string;
+	IgnoreFiles: string[];
+	Debug?:SA4UDebug;
+	constructor(config: any) {
+		if (config !== '') {
+			this.ProtocolDefinitionFile = config.MessageDefinition || config.ProtocolDefinitionFile;
+			this.CompilationDir = config.CompilationDir;
+			this.PriorTypes = config.PriorTypes;
+			if (typeof config.IgnoreFiles === 'string') {
+				if (config.IgnoreFiles.replace(/\s/g, '') !== '') {
+					this.IgnoreFiles = config.IgnoreFiles.split('\n');
+				} else {
+					this.IgnoreFiles = [];
+				}
+			} else {
+				this.IgnoreFiles = config.IgnoreFiles || [];
+			}
+			if (config.DebugMode === true || config.Debug) {
+				this.Debug = new SA4UDebug(config.DebugModeDockerImage || config.Debug?.Image, config.DebugModeMaintainContainerOnExit || config.Debug?.MaintainContainerOnExit);
+			}
 		} else {
-			this.ignoreFilesCommand = '';
+			this.ProtocolDefinitionFile = '';
+			this.CompilationDir = '';
+			this.PriorTypes = '';
+			this.IgnoreFiles = [];
 		}
-	} 
+	}
+	updateVSCodeConfig(): void {
+		connection.sendNotification('UpdateConfig', { param: 'SA4U.MessageDefinition', value: this.ProtocolDefinitionFile });
+		connection.sendNotification('UpdateConfig', { param: 'SA4U.CompilationDir', value: this.CompilationDir });
+		connection.sendNotification('UpdateConfig', { param: 'SA4U.PriorTypes', value: this.PriorTypes });
+		connection.sendNotification('UpdateConfig', { param: 'SA4U.IgnoreFiles', value: this.IgnoreFiles?.join('\n') || '' });
+		if (this.Debug) {
+			connection.sendNotification('UpdateConfig', { param: 'SA4U.DebugMode', value: true });
+			connection.sendNotification('UpdateConfig', { param: 'SA4U.DebugModeDockerImage', value: this.Debug.Image });
+			connection.sendNotification('UpdateConfig', { param: 'SA4U.DebugModeMaintainContainerOnExit', value: this.Debug.MaintainContainerOnExit });
+		}
+	}
+	async writeToJSON(): Promise<void> {
+		try {
+			await promises.writeFile(CONFIG_FILE, JSON.stringify(this, null, '    '));
+		} catch (err) {
+			console.warn(`Failed to write to ${CONFIG_FILE}: ${err}`);
+		}
+	}
+	returnParameters(folder: WorkspaceFolder): string {
+		const path = getPath(folder, true);
+		return `${(this.Debug?.MaintainContainerOnExit) ? '' : '--rm '}--mount type=bind,source="${path}",target="/src/" --name sa4u_z3_server_${path.replace(/([^A-Za-z0-9]+)/g, '')} ${this.Debug?.Image ?? dockerImageName} -d True -c "/src/${this.CompilationDir}" ${(this.IgnoreFiles.length === 0) ? '' : '-i '.concat(this.IgnoreFiles.join(' -i '))} -p /src/${this.PriorTypes} -m /src/${this.ProtocolDefinitionFile} --serialize-analysis /src/.sa4u/cache`;
+	}
 }
 
 async function checkForDockerRunningAndInstalled(): Promise<void> {
@@ -74,41 +121,81 @@ async function startDockerContainer() {
 	}
 }
 
-async function stopDockerContainer(): Promise<void> {
+async function restartDockerContainer(): Promise<void> {
+	if (startedSA4U_Z3) {
+		startedSA4U_Z3 = false;
+		const folders = await connection.workspace.getWorkspaceFolders();
+		if (folders) {
+			folders.forEach(async (folder) => {
+				const path = getPath(folder);
+				try {
+					await execAsync(`docker container stop sa4u_z3_server_${path}`);
+				} catch (err) {
+					console.log(`Failed to stop the docker container: ${err}`);
+				}
+				try {
+					await execAsync(`docker container rm sa4u_z3_server_${path}`);
+				} catch (err) {
+					console.log(`Failed to remove a stopped docker container: ${err}`);
+				}
+				dockerContainer(folder);
+			});
+		}
+	}
+}
+
+async function stopAndRemoveDockerContainer(): Promise<void> {
 	let container_ids;
 	try {
-		container_ids = (await execAsync(`docker container ls -q -f name=sa4u_z3_server`)).stdout.split('\n').join(' ');
+		container_ids = (await execAsync(`docker container ls -a -q -f name=sa4u_z3_server`)).stdout.split('\n').join(' ');
 		await execAsync(`docker container stop ${container_ids}`);
 	} catch (err) {
 		console.log(err);
 	}
+	try {
+		await execAsync(`docker container rm ${container_ids}`);
+	} catch (err) {
+		console.log(`Failed to remove a stopped docker container: ${err}`);
+	}
 }
 
-async function getSA4UConfig(): Promise<SA4UConfig> {
+async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+	const folders = await connection.workspace.getWorkspaceFolders();
+	if (folders) {
+		folders.forEach(async (folder) => {
+			const path = getPath(folder);
+			try {
+				await execAsync(`docker container kill -s=HUP sa4u_z3_server_${path}`);
+			} catch (err) {
+				console.warn(`Failed to send HUP signal to the contianer: ${err}`);
+			}
+		});
+	}
+}
+
+async function getConfig() {
 	let readConfig: string;
 	try {
-		readConfig = (await promises.readFile('.sa4u.json')).toString();
+		readConfig = (await promises.readFile(CONFIG_FILE)).toString();
 	} catch (err) {
-		console.warn('unable to read SA4U config: ' + err);
-		return new SA4UConfig('');
+		throw `Unable to read ${CONFIG_FILE}: ${err}`;
 	}
 	let configJSON;
 	try {
 		configJSON = JSON.parse(readConfig);
 	} catch (err) {
-		console.warn(`Failed to parse .sa4u.json as a JSON: ${err}`);
-		return new SA4UConfig('');
+		throw `Failed to parse ${CONFIG_FILE} as a JSON: ${err}`;
 	}
-	if (configJSON['CompilationDir'] === undefined) {
+	return configJSON;
+}
+
+async function getSA4UConfig(): Promise<SA4UConfig> {
+	let configJSON;
+	try {
+		configJSON = await getConfig();
+	} catch (err) {
+		console.warn(`${err}`);
 		return new SA4UConfig('');
-	}
-	if (!existsSync('.sa4u')){
-		try {
-			await promises.mkdir('.sa4u');
-		} catch (err) {
-			console.warn(`Failed to create .sa4u directory: ${err}`);
-			return new SA4UConfig(configJSON['CompilationDir']);
-		}
 	}
 	const directory = resolve('./');
 	let readCompileCommands;
@@ -120,52 +207,41 @@ async function getSA4UConfig(): Promise<SA4UConfig> {
 	}
 	try {
 		await promises.writeFile('.sa4u/compile_commands.json', readCompileCommands);
-		return new SA4UConfig('.sa4u/');
+		configJSON['CompilationDir'] = '.sa4u/';
+		return new SA4UConfig(configJSON);
 	} catch (err) {
 		console.warn(`Failed to write compile_commands.json into .sa4u directory: ${err}`);
-		return new SA4UConfig(configJSON['CompilationDir']);
+		return new SA4UConfig(configJSON);
 	}
 }
 
-async function getIgnoredFiles(): Promise<SA4UIgnoredFiles> {
-	let readConfig: string;
-	try {
-		readConfig = (await promises.readFile('.sa4u.json')).toString();
-	} catch (err) {
-		console.warn('unable to read SA4U config: ' + err);
-		return new SA4UIgnoredFiles([]);
-	}
-	let configJSON;
-	try {
-		configJSON = JSON.parse(readConfig);
-		if (configJSON['IgnoreFiles'] === undefined) {
-			return new SA4UIgnoredFiles([]);
+async function updateConfigurations(): Promise<void> {
+	if (!(await promises.access('.sa4u/', constants.F_OK).then(() => true, () => false))) {
+		try {
+			await promises.mkdir('.sa4u');
+		} catch (err) {
+			console.warn(`Failed to create .sa4u directory: ${err}`);
 		}
-		return new SA4UIgnoredFiles(configJSON['IgnoreFiles']);
+	}
+	if (!(await promises.access(CONFIG_FILE, constants.F_OK).then(() => true, () => false))) {
+		const sa4uJSON = new SA4UConfig(await connection.workspace.getConfiguration('SA4U'));
+		await sa4uJSON.writeToJSON();
+	}
+	try {
+		const sa4uJSON = new SA4UConfig(await getConfig());
+		sa4uJSON.updateVSCodeConfig();
 	} catch (err) {
-		console.warn(`Failed to parse .sa4u.json as a JSON: ${err}`);
-		return new SA4UIgnoredFiles([]);
+		console.warn(`${err}`);
 	}
 }
 
-async function updateMessageDefinitionFromFile(): Promise<void> {
-	try {
-		const readFile = (await promises.readFile('.sa4u.json')).toString();
-		const configJSON = JSON.parse(readFile);
-		messDef = (configJSON['ProtocolDefinitionFile']?configJSON['ProtocolDefinitionFile']:'');
-	} catch (err) {
-		console.warn(`Failed to read .sa4u.json: ${err}`);
-	}
-	if (messDef == '') {
-		const config = await connection.workspace.getConfiguration('SA4U');
-		if (config.messageDefinition)
-			messDef = config.messageDefinition;
-	}
+async function changedConfiguration(change: DidChangeConfigurationParams): Promise<void> {
+	await (new SA4UConfig(change.settings.SA4U)).writeToJSON();
+	restartDockerContainer();
 }
 
 async function dockerContainer(folder: WorkspaceFolder): Promise<void> {
 	const filePath = decodeURIComponent(folder.uri);
-	const path = getPath(folder, true);
 	const diagnosticsMap = new Map<string, Diagnostic[]>();
 	try {
 		// eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -229,9 +305,8 @@ async function dockerContainer(folder: WorkspaceFolder): Promise<void> {
 				}
 			}
 		};
-		const sa4uConfig = await Promise.all([getSA4UConfig(), getIgnoredFiles()]).then((value) => {return {compilationDir: value[0].compilationDir, ignore: value[1].ignoreFilesCommand};});
-		console.log(`using compile dir: ${sa4uConfig.compilationDir}`);
-		const child = exec(`docker container run --rm --mount type=bind,source="${path}",target="/src/" --name sa4u_z3_server_${path.replace(/([^A-Za-z0-9]+)/g, '')} ${dockerImageName} -d True -c "${sa4uConfig.compilationDir}" ${sa4uConfig.ignore} -p /src/ex_prior.json -m /src/${messDef}`);
+		const sa4uConfig = await getSA4UConfig();
+		const child = exec(`docker container run ${sa4uConfig.returnParameters(folder)}`);
 		const rl = readline.createInterface({input: child.stdout});
 		rl.on('line', (line: any) => {
 			console.log(line);
@@ -247,51 +322,6 @@ async function dockerContainer(folder: WorkspaceFolder): Promise<void> {
 		startedSA4U_Z3 = true;
 	} catch (e) {
 		connection.sendNotification('ServerError', `Failed to start the docker container: ${e}`);
-	}
-}
-
-async function changedConfiguration(change: DidChangeConfigurationParams): Promise<void> {
-	messDef = change.settings.SA4U.messageDefinition;
-	try {
-		const readSA4U = (await promises.readFile('.sa4u.json')).toString();
-		const configJSON = JSON.parse(readSA4U);
-		configJSON['ProtocolDefinitionFile'] = messDef;
-		await promises.writeFile('.sa4u.json', JSON.stringify(configJSON, null, '    '));
-	} catch (err) {
-		console.warn(`Failed to update .sa4u file: ${err}`);
-	}
-	restartDockerContainer();
-}
-
-async function restartDockerContainer(): Promise<void> {
-	if (startedSA4U_Z3) {
-		startedSA4U_Z3 = false;
-		const folders = await connection.workspace.getWorkspaceFolders();
-		if (folders) {
-			folders.forEach(async (folder) => {
-				const path = getPath(folder);
-				try {
-					await execAsync(`docker container stop sa4u_z3_server_${path}`);
-				} catch (err) {
-					console.log(`Failed to stop the docker container: ${err}`);
-				}
-				dockerContainer(folder);
-			});
-		}
-	}
-}
-
-async function validateTextDocument(textDocument: TextDocument): Promise<void> {
-	const folders = await connection.workspace.getWorkspaceFolders();
-	if (folders) {
-		folders.forEach(async (folder) => {
-			const path = getPath(folder);
-			try {
-				await execAsync(`docker container kill -s=HUP sa4u_z3_server_${path}`);
-			} catch (err) {
-				console.warn(`Failed to send HUP signal to the contianer: ${err}`);
-			}
-		});
 	}
 }
 
@@ -339,18 +369,19 @@ connection.onInitialize((params: InitializeParams) => {
 
 connection.onInitialized(() => {
 	allowConfigurationChanges = false;
-	if (hasConfigurationCapability) {
-		// Register for all configuration changes.
-		connection.client.register(DidChangeConfigurationNotification.type, undefined);
-		
-	}
 	if (hasWorkspaceFolderCapability) {
 		connection.workspace.onDidChangeWorkspaceFolders(_event => {
 			connection.console.log('Workspace folder change event received.');
 		});
 	}
-	Promise.all([checkForDockerRunningAndInstalled(), updateMessageDefinitionFromFile(), stopDockerContainer()])
-		.then(() => {startDockerContainer();}, (err) => {connection.sendNotification('ServerError', `Server Failed to start Docker container: ${err}`);});
+	Promise.all([checkForDockerRunningAndInstalled(), updateConfigurations(), stopAndRemoveDockerContainer()]).then(
+		() => {startDockerContainer();}, 
+		(err) => {connection.sendNotification('ServerError', `Server Failed to start Docker container: ${err}`);}
+	);
+	if (hasConfigurationCapability) {
+		// Register for all configuration changes.
+		connection.client.register(DidChangeConfigurationNotification.type, undefined);
+	}
 });
 
 connection.onDidChangeConfiguration(change => {
